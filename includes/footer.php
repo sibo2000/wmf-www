5c59d4ef3f93b007d3da6afd42414c1edaa8cb9e
simonford <simonford4@gmail.com>
2013-05-13 14:17:15 +0100

    Footer changes (wip)
    Signed-off-by: simonford <simonford4@gmail.com>

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,38 @@
-    <footer>
-      <nav>
-        <ul class="mod-nav-first">
-          <li><a href="">Home</a></li>
-          <li><a href="">Stay at the farm</a></li>
-          <li><a href="">About the farm</a></li>
-          <li><a href="">Reviews</a></li>
-          <li><a href="">Attractions</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">Book now</a></li>
-        </ul>
-        <ul class="mod-nav-second">
-          <li><a href="">Home</a></li>
-          <li><a href="">Stay at the farm</a></li>
-          <li><a href="">About the farm</a></li>
-          <li><a href="">Reviews</a></li>
-          <li><a href="">Attractions</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">Book now</a></li>
-        </ul>
-        <ul class="mod-nav-last">
-          <li><a href="">Home</a></li>
-          <li><a href="">Stay at the farm</a></li>
-          <li><a href="">About the farm</a></li>
-          <li><a href="">Reviews</a></li>
-          <li><a href="">Attractions</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">Book now</a></li>
-        </ul>
-      </nav>
-    </footer>
+   
   </div>
+<footer>
+<div class="l-pagewrap">
+  <nav>
+    <ul class="mod-nav-first">
+      <li><h4><a href="#">Header text</a></h4></li>
+			<li><a href="">Home</a></li>
+      <li><a href="">Stay at the farm</a></li>
+      <li><a href="">About the farm</a></li>
+      <li><a href="">Reviews</a></li>
+      <li><a href="">Attractions</a></li>
+      <li><a href="">Contact</a></li>
+      <li><a href="">Book now</a></li>
+    </ul>
+    <ul class="mod-nav-second">
+      <li><a href="">Home</a></li>
+      <li><a href="">Stay at the farm</a></li>
+      <li><a href="">About the farm</a></li>
+      <li><a href="">Reviews</a></li>
+      <li><a href="">Attractions</a></li>
+      <li><a href="">Contact</a></li>
+      <li><a href="">Book now</a></li>
+    </ul>
+    <ul class="mod-nav-last">
+      <li><a href="">Home</a></li>
+      <li><a href="">Stay at the farm</a></li>
+      <li><a href="">About the farm</a></li>
+      <li><a href="">Reviews</a></li>
+      <li><a href="">Attractions</a></li>
+      <li><a href="">Contact</a></li>
+      <li><a href="">Book now</a></li>
+    </ul>
+  </nav>
+</div>
+</footer>
 </body>
 </html>
