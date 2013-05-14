103779b172603272f143ae024150f3268e75d989
simonford <simonford4@gmail.com>
2013-05-14 10:01:29 +0100

    typo in subnav
    Signed-off-by: simonford <simonford4@gmail.com>

--- includes/nav_accom.php
+++ includes/nav_accom.php
@@ -1,7 +1,7 @@
 <nav class="mod-subNav">
   <ul>
     <li><a href="./stay-at-the-farm/camping.php">Camping</a></li>
-    <li><a class="active" href="./stay-at-the-farm/caravaning.php">Caravaning</a></li>
+    <li><a class="active" href="./stay-at-the-farm/caravaning.php">Caravanning</a></li>
     <li><a href="./stay-at-the-farm/log-pods.php">Log pods</a></li>
     <li><a href="./stay-at-the-farm/bed-and-breakfast.php">Bed &amp; Breakfast</a></li>
   </ul>
