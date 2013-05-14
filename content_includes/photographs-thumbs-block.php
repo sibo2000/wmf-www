fbaa74ee67010b71feee8cd5b42b6505ccbc0400
simonford <simonford4@gmail.com>
2013-05-14 15:40:49 +0100

    photo block css mods
    Signed-off-by: simonford <simonford4@gmail.com>

---
+++ content_includes/photographs-thumbs-block.php
@@ -0,0 +1,25 @@
+<h3>Photographs</h3>
+  <div class="imageBox">
+       <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+      <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+       <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+      <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+       <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+      <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+       <img src="../img/photo-thumb.png">
+  </div>
+  <div class="imageBox">
+      <img src="../img/photo-thumb.png">
+  </div>
