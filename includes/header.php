db7476e61f28b6bc6c08c32a3ebde9bc068f5d36
simonford <simonford4@gmail.com>
2013-05-14 18:17:04 +0100

    changes to header to include social icons
    Signed-off-by: simonford <simonford4@gmail.com>

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,14 @@
   <div class="l-pagewrap">
     <header>
       <h1 class="logo">West Middlewick Farm</h1>
-      <a class="phoneNumber" href="tel:0700 879234">01884 861235</a>
+      <div id="channels">
+        <ul>
+          <li><a class="phoneNumber" href="tel:01884 861235">01884 861235</a></li>
+          <li><a title="Check out Flickr images" href="#"><img src="../img/flickr-icon.png" alt="Flickr"></a></li>
+          <li><a title="Follow us on Twitter" href="#"><img src="../img/twitter-icon.png" alt="Twitter"></a></li>
+          <li><a title="Follow us on Facebook" href="#"><img src="../img/fb-icon.png" alt="Facebook"></a></li>
+    	  </ul>
+    	</div>
       <?php include('nav_main.php'); ?>
       <?php if ($subNav == "nav-accom") { include('nav_accom.php'); }; ?> 
       
