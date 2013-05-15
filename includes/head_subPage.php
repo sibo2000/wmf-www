735379459554d38669cb668790488c9c179c67d7
simonford <simonford4@gmail.com>
2013-05-15 13:54:16 +0100

    added tabbed content component
    Signed-off-by: simonford <simonford4@gmail.com>

--- includes/head_subPage.php
+++ includes/head_subPage.php
@@ -15,5 +15,7 @@
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
   <link rel="stylesheet" href="../fancybox/jquery.fancybox.css" type="text/css" media="screen" />
   <script type="text/javascript" src="../fancybox/jquery.fancybox.pack.js"></script>
-			
+  
+  
+  		
 </head>
--- stay-at-the-farm/log-pods.php
+++ stay-at-the-farm/log-pods.php
@@ -4,6 +4,33 @@
 <section class="l-primary">
   <h1>Log pods on a <em>working farm</em> in <em>rural Devon</em>.</h1>     
   <img src="" alt="lead image " />   
+  <div id="tabbed_box_1" class="tabbed_box">
+    <h4>Heading</h4>
+    <div class="tabbed_area">
+        <script src="../includes/functions.js" type="text/javascript"></script>   
+        <ul class="tabs">  
+            <li><a href="javascript:tabSwitch('tab_1', 'content_1');" id="tab_1" class="active">Prices</a></li>  
+            <li><a href="javascript:tabSwitch('tab_2', 'content_2');" id="tab_2">Facilities</a></li>  
+            <li><a href="javascript:tabSwitch('tab_3', 'content_3');" id="tab_3">Pages</a></li>  
+        </ul>
+
+                <div id="content_1" class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut molestie, augue in scelerisque commodo, nulla magna hendrerit tortor, et accumsan libero mauris bibendum leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus lectus, convallis faucibus ullamcorper condimentum, consectetur aliquam tellus. Fusce in rhoncus ligula. Cras neque dui, accumsan vitae dignissim vitae, vestibulum id arcu. Maecenas risus nulla, euismod id ornare a, bibendum vel nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat, nunc quis malesuada molestie, erat felis tempus odio, sodales auctor odio odio in nisl. Integer odio nunc, elementum ac semper porttitor, adipiscing vel felis. Vestibulum id magna eros, at ultrices odio. Suspendisse iaculis rhoncus mi et dictum. Nulla enim diam, lobortis vel vehicula vel, egestas in purus. Sed eu eros sodales ligula adipiscing dignissim.</p>
+
+                  <p>Vestibulum aliquet varius enim, et accumsan magna placerat auctor. Duis gravida varius ullamcorper. Phasellus vitae eros odio, ac luctus metus. Integer imperdiet ultricies odio, at rhoncus ante ultrices vitae. Phasellus ac tortor libero. Fusce ut sagittis dolor. Etiam sed lacus vitae orci vehicula porttitor. Duis fringilla dictum odio. Integer nec tellus diam, eu mattis urna. Curabitur risus quam, dapibus non scelerisque et, hendrerit sed urna. Maecenas ut ipsum vitae lacus molestie placerat et eget felis.</p>
+                </div>  
+                <div id="content_2" class="content">
+                  <p>TWO TTWO dolor sit amet, consectetur adipiscing elit. Ut molestie, augue in scelerisque commodo, nulla magna hendrerit tortor, et accumsan libero mauris bibendum leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus lectus, convallis faucibus ullamcorper condimentum, consectetur aliquam tellus. Fusce in rhoncus ligula. Cras neque dui, accumsan vitae dignissim vitae, vestibulum id arcu. Maecenas risus nulla, euismod id ornare a, bibendum vel nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat, nunc quis malesuada molestie, erat felis tempus odio, sodales auctor odio odio in nisl. Integer odio nunc, elementum ac semper porttitor, adipiscing vel felis. Vestibulum id magna eros, at ultrices odio. Suspendisse iaculis rhoncus mi et dictum. Nulla enim diam, lobortis vel vehicula vel, egestas in purus. Sed eu eros sodales ligula adipiscing dignissim.</p>
+
+                  <p>Vestibulum aliquet varius enim, et accumsan magna placerat auctor. Duis gravida varius ullamcorper. Phasellus vitae eros odio, ac luctus metus. Integer imperdiet ultricies odio, at rhoncus ante ultrices vitae. Phasellus ac tortor libero. Fusce ut sagittis dolor. Etiam sed lacus vitae orci vehicula porttitor. Duis fringilla dictum odio. Integer nec tellus diam, eu mattis urna. Curabitur risus quam, dapibus non scelerisque et, hendrerit sed urna. Maecenas ut ipsum vitae lacus molestie placerat et eget felis.</p>
+                </div>  
+                <div id="content_3" class="content">
+                  <p>LTHREEsit amet, consectetur adipiscing elit. Ut molestie, augue in scelerisque commodo, nulla magna hendrerit tortor, et accumsan libero mauris bibendum leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus lectus, convallis faucibus ullamcorper condimentum, consectetur aliquam tellus. Fusce in rhoncus ligula. Cras neque dui, accumsan vitae dignissim vitae, vestibulum id arcu. Maecenas risus nulla, euismod id ornare a, bibendum vel nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat, nunc quis malesuada molestie, erat felis tempus odio, sodales auctor odio odio in nisl. Integer odio nunc, elementum ac semper porttitor, adipiscing vel felis. Vestibulum id magna eros, at ultrices odio. Suspendisse iaculis rhoncus mi et dictum. Nulla enim diam, lobortis vel vehicula vel, egestas in purus. Sed eu eros sodales ligula adipiscing dignissim.</p>
+
+                  <p>Vestibulum aliquet varius enim, et accumsan magna placerat auctor. Duis gravida varius ullamcorper. Phasellus vitae eros odio, ac luctus metus. Integer imperdiet ultricies odio, at rhoncus ante ultrices vitae. Phasellus ac tortor libero. Fusce ut sagittis dolor. Etiam sed lacus vitae orci vehicula porttitor. Duis fringilla dictum odio. Integer nec tellus diam, eu mattis urna. Curabitur risus quam, dapibus non scelerisque et, hendrerit sed urna. Maecenas ut ipsum vitae lacus molestie placerat et eget felis.</p>
+                </div>
+    </div>
+  </div>
 </section>      
 <section class="l-secondary">
  <div id="photo-thumbs"> 
@@ -40,7 +67,7 @@
   </script>
   <div class="readytobook">
     <h3>Ready to book</h3>
-    <p>If you would like to book a pitch or have any questions please email us at <a href="mailto:stay@westmiddlewick.co.uk">Stay@westmiddlewick.co.uk</a> or telephone: 01884 861 235. Or you can use form below.</p>
+    <p>If you would like to book a pitch or have any questions please email us at <a href="mailto:stay@westmiddlewick.co.uk">Stay@ westmiddlewick.co.uk</a> or telephone: 01884 861 235. Or you can use form below.</p>
     <a class="button" href="">Booking Form ></a>
     
   </div>
