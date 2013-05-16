9bd3c7e4cfdb7c3ee7a0880c92aa614beeb89c47
simonford <simonford4@gmail.com>
2013-05-16 08:37:06 +0100

    mod-add-block changes
    Signed-off-by: simonford <simonford4@gmail.com>

--- content_includes/mod_add_block.php
+++ content_includes/mod_add_block.php
@@ -12,5 +12,10 @@
 </div>
 <div class="mod-adBlock">
   <h4>Local events</h4>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>        
+  <ul>
+    <li><a href="#">Tiverton Balloon festival</a></li>
+    <li><a href="#">Devon farm awards</a></li>
+    <li><a href="#">Ploughing competition</a></li>
+    <li><a href="#">Tractor Pull</a></li>     
+  </ul>   
 </div>
