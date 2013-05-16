c1be767630f4b62ad62f64d273ee429863a32896
simonford <simonford4@gmail.com>
2013-05-16 08:36:53 +0100

    flickr url added
    Signed-off-by: simonford <simonford4@gmail.com>

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
       <div id="channels">
         <ul>
           <li><a class="phoneNumber" href="tel:01884 861235">01884 861235</a></li>
-          <li><a title="Check out Flickr images" href="#"><img src="../img/flickr-icon.png" alt="Flickr"></a></li>
+          <li><a title="Check out Flickr images" href="http://www.flickr.com/photos/westmiddlewick/"><img src="../img/flickr-icon.png" alt="Flickr"></a></li>
           <li><a title="Follow us on Twitter" href="https://twitter.com/westmiddlewick"><img src="../img/twitter-icon.png" alt="Twitter"></a></li>
           <li><a title="Follow us on Facebook" href="https://www.facebook.com/pages/West-Middlewick-Farm/123773327794960"><img src="../img/fb-icon.png" alt="Facebook"></a></li>
     	  </ul>
