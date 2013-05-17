d05324c3253ad005427b9f5097ab2aa96d778cea
simonford <simonford4@gmail.com>
2013-05-17 15:50:06 +0100

    5 promos added
    needs some attention for the responsive side. SF to pick up unless you get the urge
    
    Signed-off-by: simonford <simonford4@gmail.com>

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <section class="l-primary">
   <h2>Ways to stay</h2>
   <div class="mod-stayBlock">
-<h3><img src="img/home-egg-1.png" alt="Egg1">Camping</h3>
+    <img class="egg" src="img/home-egg-1.png" alt="Egg1">
+<h3>Camping</h3>
     <p>Situated in a 4 acre field at the end of a stone track with stunning views over Exmoor.  </p>
     <div class="mod-stayBlock-img">
       <img src="img/mod-stayBlock-ex-img.png">
@@ -15,20 +16,41 @@
     </div>
   </div>
   <div class="mod-stayBlock">
-<h3>Caravanning</h3>
-    <p>2 cosy little log-pods available for hire, a perfect weekend away in the country.weekend away in the country. </p>        
+     <img class="egg" src="img/home-egg-2.png" alt="Egg2">
+<h3><a href="#">Caravanning</a></h3>
+    <p>2 cosy little log-pods available for hire, a perfect weekend away in the country. </p> 
+    <div class="mod-stayBlock-img">
+      <img src="img/caravanning-thumb.png">
+      <a href="#" class="button">Come caravanning &gt;</a>
+    </div>       
   </div>
   <div class="mod-stayBlock">
-<h3>Log pods - come glamping!</h3>
-    <p>2 cosy little log-pods available for hire, a perfect weekend away in the country.weekend away in the country. </p>        
+     <img class="egg" src="img/home-egg-3.png" alt="Egg3">
+<h3><a href="#">Log pods - come glamping!</a></h3>
+    <p>2 cosy little log-pods available for hire, a perfect weekend away in the country </p>  
+    <div class="mod-stayBlock-img">
+      <img src="img/logpod-thumb.png">
+      <a href="#" class="button">Come glamping &gt;</a>
+    </div>      
   </div>
   <div class="mod-stayBlock">
-<h3>Bed &amp; breakfast</h3>
+     <img class="egg" src="img/home-egg-4.png" alt="Egg4">
+<h3><a href="#">Bed &amp; breakfast</a></h3>
     <p>2 cosy little log-pods available for hire, a perfect weekend away in the country. </p>        
+    <div class="mod-stayBlock-img">
+      <img src="img/bb-thumb.png">
+      <a href="#" class="button">Stay B&amp;B &gt;</a>
+    </div>
   </div>
   <div class="mod-stayBlock mod-stayBlockDouble">
-    <h3>Stay in a log cabin</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>        
+     <img class="egg" src="img/home-egg-5.png" alt="Egg5">
+    <h3><a href="#">Stay in a log cabin</a></h3>
+<p>There are some of those among us who love the countryside but maybe 
+    aren't enamoured by the thought of staying on a campsite! </p>  
+    <img class="doublephoto" src="img/log-cabin-thumb.png">
+    <p>Now theres the perfect self catering solution at West Middlewick</p>  
+     <a href="#" class="doublebutton">Stay in a log cabin &gt;</a>
+     <p class="buttontext">(Launches new site)</p>
   </div>
 </section>         
 <section class="l-secondary">
