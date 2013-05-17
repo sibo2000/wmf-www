151c1e91dffd80ccd553129111de211767584fdd
simonford <simonford4@gmail.com>
2013-05-17 18:15:41 +0100

    chnages to adblocks
    Signed-off-by: simonford <simonford4@gmail.com>

--- content_includes/mod_add_block.php
+++ content_includes/mod_add_block.php
@@ -1,17 +1,20 @@
 <div class="mod-adBlock">
   <h4>About us</h4>
   <img src="img/about-image.png">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>        
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>    
+  <p class="tractor"><img src="img/tractor-icon.png"><a href="#">The article link</a></p>     
 </div>
 <div class="mod-adBlock">
   <h4>Where are we</h4>
   <img src="img/about-image.png">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>        
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>     
+  <p class="tractor"><img src="img/tractor-icon.png"><a href="#">The article link</a></p>   
 </div>  
 <div class="mod-adBlock">
   <h4>Reviews</h4>
   <img src="img/about-image.png">
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, repellendus, eligendi, iusto distinctio debitis praesentium natus officia odit </p>        
+  <p class="tractor"><img src="img/tractor-icon.png"><a href="#">The article link</a></p> 
 </div>
 <div class="mod-adBlock">
   <h4>Local events</h4>
